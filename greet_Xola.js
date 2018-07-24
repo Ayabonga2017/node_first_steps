fa335f96ebd494f339fd04ed83f2a4c324d74518
Ayabonga <nompetsheniayabonga@gmail>
2018-07-24 12:51:08 +0200

    added figlet

--- greet_Xola.js
+++ greet_Xola.js
@@ -4,9 +4,10 @@ const greet = require('./greet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
+
 var figlet = require('figlet');
 
-figlet('Hello Xola!!', function(err, data) {
+figlet(greet("Xola"), function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
